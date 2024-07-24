345cb01f89f3f65782d16e609e8d079ab5858e4d
Jakub Stasiak <jakub.stasiak.front@gmail.com>
2024-07-24 16:37:07 +0200

    added handling of the api key, added more logging steps

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
+const vision = require('@google-cloud/vision');
 
 
 const app = express();
 const port = 3000;
+const client = new vision.ImageAnnotatorClient({
+    keyFilename: './importantKey.json'
+});
 
 app.use(cors());
 
@@ -20,9 +24,17 @@ const upload = multer({storage: storage});
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.post('/uploads', upload.single('screenshot'), (req, res) => {
+app.post('/uploads', upload.single('screenshot'), async (req, res) => {
     try {
-        res.json({message: 'Screenshot uploaded !', file: req.file });
+        console.log(req.file);
+        const fileRef = req.file.destination + req.file.filename;
+        console.log(fileRef);
+        const [result] = await client.textDetection(fileRef); // line that causes error and stops request from executing any further
+        console.log(result)
+        const detections = result.textAnnotations;
+        console.log(detections);
+
+        res.json({message: 'Screenshot uploaded !', file: req.file, detections: detections });
     } catch (err) {
         res.status(400).send('Error uploading file');
     }
