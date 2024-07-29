ed99825a50599fb492d9b3d545834079579656a4
Jakub Stasiak <jakub.stasiak.front@gmail.com>
2024-07-29 16:13:57 +0200

    added file with all tradable items in the game

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
 const vision = require('@google-cloud/vision');
+const axios = require('axios')
+const allTradableItems = require('./TradableItems.js')
 
 
 const app = express();
 const port = 3000;
 const client = new vision.ImageAnnotatorClient({
-    keyFilename: './importantKey.json'
+    keyFilename: './importantKey.json' // file with your google cloud service account info
 });
 
 app.use(cors());
 
+// Handle image file storage provided by the user
 const storage = multer.diskStorage({
     destination: './uploads/',
     filename: (req, file, cb) => {
@@ -24,6 +27,7 @@ const upload = multer({storage: storage});
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// handle request to upload screenshot and extract text from it
 app.post('/uploads', upload.single('screenshot'), async (req, res) => {
     try {
         console.log(req.file);
@@ -34,6 +38,7 @@ app.post('/uploads', upload.single('screenshot'), async (req, res) => {
         const detections = result.fullTextAnnotation;
         console.log(detections);
 
+        // process data sent from cloud vision api to group detections properly
         let phrases = [];
         if (detections && detections.pages) {
             detections.pages.forEach(page => {
@@ -55,6 +60,11 @@ app.post('/uploads', upload.single('screenshot'), async (req, res) => {
         res.status(400).send('Error uploading file');
     }
 });
+
+app.get('/tradable-items', async (req, res) => {
+    console.log(allTradableItems);
+    res.json('done');
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
